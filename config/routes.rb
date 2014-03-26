676bb3d7d432e3ec1a6dc611196928ed8e37a4aa
Peter Barnes <barnespeter213@gmail.com>
2014-03-26 14:17:17 -0500

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 RepairsSampleApp::Application.routes.draw do
-  get "users/new"
+  resources :users
   root 'static_pages#home'
   match '/help', to: 'static_pages#help', via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
